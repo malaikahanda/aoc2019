28c4ba6883482d23832709e75a7bea9f2c2326f7
Malaika Handa <mhanda@andrew.cmu.edu>
2019-12-01 01:40:06 -0500

    part2

--- day01/sum.py
+++ day01/sum.py
@@ -1,3 +1,4 @@
+
 # Part 1
 
 total = 0
@@ -6,5 +7,27 @@ with open("input.txt") as f:
     for line in f.readlines():
         i = int(line)
         total += ((i // 3) - 2)
+
+print("Total:", total)
+
+
+# Part 2
+
+def get_fuel(x, total_so_far=0):
+
+    diminished = (x // 3) - 2
+
+    if diminished <= 0:
+        return total_so_far
+
+    else:
+        return get_fuel(diminished, total_so_far + diminished)
+
+total = 0
+
+with open("input.txt") as f:
+    for line in f.readlines():
+        i = int(line)
+        total += get_fuel(i)
 
 print("Total:", total)
