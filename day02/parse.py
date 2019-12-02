ab6ab893cf6bcfc80b0676bd5db42f538d8558bb
Malaika Handa <mhanda@andrew.cmu.edu>
2019-12-02 01:21:07 -0500

    bruteforceeee

--- day02/parse.py
+++ day02/parse.py
@@ -1,4 +1,6 @@
 
+import copy
+
 def parse_intcode(intcode):
 
     counter = 0
@@ -12,8 +14,16 @@ def parse_intcode(intcode):
         else:
 
             pos1 = intcode[counter + 1]
+            if pos1 >= len(intcode):
+                return []
+
             pos2 = intcode[counter + 2]
+            if pos2 >= len(intcode):
+                return []
+
             pos_out = intcode[counter + 3]
+            if pos_out >= len(intcode):
+                return []
 
             if opcode == 1:
                 intcode[pos_out] = intcode[pos1] + intcode[pos2]
@@ -35,11 +45,26 @@ if __name__ == "__main__":
         list_intcode = text_intcode.split(",")
         intcode = [int(i) for i in list_intcode]
 
-        intcode[1] = 12
-        intcode[2] = 2
-
-        out_intcode = parse_intcode(intcode)
-
+        ## Part 1.
+        input_intcode = copy.copy(intcode)
+        input_intcode[1] = 12
+        input_intcode[2] = 2
+        out_intcode = parse_intcode(input_intcode)
         print("At position 0:", out_intcode[0])
+
+        ## Part 2:
+        for noun in range(100):
+            for verb in range(100):
+
+                input_intcode = copy.copy(intcode)
+                input_intcode[1] = noun
+                input_intcode[2] = verb
+
+                out_intcode = parse_intcode(input_intcode)
+
+                if out_intcode and out_intcode[0] == 19690720:
+                    print("noun =", noun)
+                    print("verb =", verb)
+                    print("100 * noun + verb =", 100 * noun + verb)
 
 
